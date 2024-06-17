85e53861514a40e2bc8897e035156e421f892305
Ливчак Константин <konstantin.livchak@fors.ru>
2024-06-17 18:02:06 +0300

    Созданые классы для изучения работы с делегатами и событиями

---
+++ MeDeligateAndEventTask/MyCollectionAnalyzer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeDeligateAndEventTask
+{
+    public static class MyCollectionAnalyzer
+    {
+        public static T GetMax<T> (this IEnumerable<T> collection, Func<T, double> convertToNumber)
+        {
+            var maxValue = double.MinValue;
+            T maxItem = default;
+
+
+            foreach (var item in collection)
+            {
+                var currentNum = convertToNumber(item);
+                if (currentNum > maxValue)
+                {
+                    maxValue = currentNum;
+                    maxItem = item;
+                }
+            }
+
+            return maxItem;
+        }
+    }
+}
---
+++ MeDeligateAndEventTask/Program.cs
@@ -0,0 +1,35 @@
+﻿// 1) Написать метод-расширение, принимающий на вход делегат в виде функции, приводящей тип в число, и возвращающий максимальный элемент кооллекции
+// 2) Написать класс, обходящий каталог файлов и выдающий собятие при нахождении каждого файла с применением public event EvendHandler и аргумент FileArgs : EventArgs
+// 3) Для 2) предусмотреть возможность остановки сканирования
+// 4) Выводить в консоль результаты возникновения событий и работы метода расширений
+
+
+using MeDeligateAndEventTask;
+
+//Тестирование с поиском по массиву int
+var testInt = new List<int> { -10, 11, 4, -3, 100, -99 };
+var myIntConverter = MyConverters.ConvertIntToDouble;
+var myIntMax = testInt.GetMax(myIntConverter);
+Console.WriteLine(myIntMax.ToString());
+
+//Тестирование с поиском по массиву string
+var testStr = new List<string> { "1", "-2", "3", "100,1" };
+var myStrConverter = MyConverters.ConvertStringToDouble;
+var myStrMax = testStr.GetMax(myStrConverter);
+Console.WriteLine(myStrMax.ToString());
+
+//Работа с событиями
+
+var publisher = new MyEventClassPublisher();
+var subscriber = new MyEventClassSubscriber();
+publisher.MyEvent += subscriber.DisplayMessage;
+
+Console.Write("Введите директорию для поиска - ");
+var directory = Console.ReadLine();
+
+Console.Write("Введите патерн поиска файла или его полное имя - ");
+var pattern = Console.ReadLine();
+
+publisher.ScanDirecory(directory, pattern);
+
+
